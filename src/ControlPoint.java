5f7f4f5d665e7be36463858869695d35c1986342
jmdick3 <john.dickens311@gmail.com>
2018-11-10 07:27:34 -0500

    GUI is about 2/3 implemented.

--- src/ControlPoint.java
+++ src/ControlPoint.java
@@ -1,2 +1,18 @@
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 public class ControlPoint {
+
+    public static void main(String[] args) {
+
+        ControlPointView v = new ControlPointView();
+        v.addWindowListener(new WindowAdapter()
+        {
+            public void windowClosing(WindowEvent e)
+            {
+                System.exit(0);
+            }
+        });
+
+    }
 }

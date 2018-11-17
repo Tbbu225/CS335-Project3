a94d9e9a696e9d84f2fa975aefb1a96b420d0fb3
tbbu225 <tbbu225@g.uky.edu>
2018-11-16 19:49:05 -0500

    Added some comments to MappedImage

--- src/MappedImage.java
+++ src/MappedImage.java
@@ -7,41 +7,85 @@ import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-
+/* MappedImage
+ *
+ * This class maintains both a buffered image and a set of control points
+ * that can used to warp an image.
+ *
+ * Authors:
+ * Tyler Burkett
+ * John Dickens
+ */
 public class MappedImage extends JPanel implements MouseListener, MouseMotionListener{
+
+    //Private members
+
+    //The actual image that is to be displayed under the
+    //control point grid
     private BufferedImage image;
+
+    //Associated images; this is used for pairing one images
+    //control points with another's
     private ArrayList<MappedImage> associatedImages;
+
+    //The control points on the image
     private ControlPoint[][] mappingPoints;
-    private int gridSize;
-    private int pointSize;
-    private int imageLength, imageHeight;
+
+    //A reference to the location of the currently
+    //moving control point and other control points
+    //associated with it
     private Point clickedPoint;
 
+    //The number of control points in one column/row
+    //of the control grid. This grid is always square
+    private int gridSize;
+
+    //The radius of each control point circle
+    private int pointSize;
+
+    //The size of the mappedImage
+    private int imageLength, imageHeight;
+
+
+    //Constructor
     public MappedImage(int size, int length, int height, int radius) {
+        //Call the JPanel constructor and set the background to be white
+        //in case no image is loaded
         super();
         setBackground(Color.WHITE);
 
+        //Set the image to null and allocate the
+        //list of associated images
         image = null;
         associatedImages = new ArrayList<>();
 
+        //Set sizes of points, the grid, and the MappedImage
+        //itself based on given paramters
         gridSize = size;
         pointSize = radius;
         imageLength = length;
         imageHeight = height;
 
+        //Allocate the grid to be the given size + 2 squared;
+        //This is done to have invisible control points that exist
+        //on the edge of the panel
         mappingPoints = new ControlPoint[gridSize + 2][gridSize + 2];
 
+        //Set the clicked point to null
         clickedPoint = null;
 
-
+        //Calculate the distance between points vertically and horizontally
         int widthInc = imageLength / (gridSize + 1);
         int heightInc = imageHeight / (gridSize + 1);
 
+        //Set the appropriate x and y values for the points on the grid
         for (int row = 0; row < mappingPoints.length; row++)
             for (int column = 0; column < mappingPoints[row].length; column++) {
 
+                //Determine the x and y value of the control point.
+                //If the point is the last one in the column or row,
+                //make sure it goes to the very edge of the image
                 int rowVal = heightInc * row;
                 int columnVal = widthInc * column;
                 if (row == heightInc * (gridSize + 1))
@@ -49,24 +93,32 @@ public class MappedImage extends JPanel implements MouseListener, MouseMotionLis
                 if (column == widthInc * (gridSize + 1))
                     columnVal = imageLength;
 
+                //Create a new control point
                 ControlPoint newPoint = new ControlPoint(columnVal, rowVal, 6);
 
+                //If the control point is not on the very edge of the image,
+                //make it visible
                 if (column != 0 && column != mappingPoints[row].length-1
                    && row != 0 && row != mappingPoints.length-1)
                     newPoint.isVisible = true;
 
+                //Add the new control point to the appropriate part of the list
                 mappingPoints[row][column] = newPoint;
             }
 
+        //Connect the control points appropriately
         formGrid();
 
+        //Draw the result
         repaint();
 
+        //Add listeners for mouse commands so that the points can be moved
         addMouseListener(this);
         addMouseMotionListener(this);
 
     }
 
+    //Copy Constructor
     public MappedImage(MappedImage mappedImage, boolean keepImage) {
         super();
         setBackground(Color.WHITE);
